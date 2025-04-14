656e56e209b16912339aaf16ec1f4e44d0a5bd93
P105XY <p105xy85@gmail.com>
2025-04-14 15:08:33 +0900

    asdfasdf

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
 
 //glfw를 사용해서 opengl을 사용하는 main 함수.
 int main()
